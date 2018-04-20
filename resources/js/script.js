d5198a967ca854dc18c0ddf45948892227f75b19
CamilleKonami <camille.lecompagnon@gmail.com>
2018-04-20 11:26:01 +0200

    remove gmaps

--- resources/js/script.js
+++ resources/js/script.js
@@ -12,7 +12,7 @@
 
 
 $(document).ready(function () {
-    
+
     /* For the sticky navigation */
     $('.js--section-features').waypoint(function (direction) {
         if (direction == "down") {
@@ -29,14 +29,14 @@ $(document).ready(function () {
     $('.js--scroll-to-plans').click(function () {
         $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);
     });
-    
+
     $('.js--scroll-to-start').click(function () {
         $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
     });
-    
-    
+
+
     /* Navigation scroll */
-    
+
     // Select all links with hashes
     $('a[href*="#"]')
       // Remove links that don't actually link to anything
@@ -45,8 +45,8 @@ $(document).ready(function () {
       .click(function (event) {
         // On-page links
         if (
-          location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-          && 
+          location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+          &&
           location.hostname == this.hostname
         ) {
           // Figure out element to scroll to
@@ -73,15 +73,15 @@ $(document).ready(function () {
           }
         }
       });
-    
-    
+
+
     /* Animations on scroll */
     $('.js--wp-1').waypoint(function () {
         $('.js--wp-1').addClass('animated fadeIn');
     }, {
         offset: '50%'
-    });    
-    
+    });
+
     $('.js--wp-2').waypoint(function () {
         $('.js--wp-2').addClass('animated fadeInUp');
     }, {
@@ -93,19 +93,19 @@ $(document).ready(function () {
     }, {
         offset: '50%'
     });
-    
+
     $('.js--wp-4').waypoint(function () {
         $('.js--wp-4').addClass('animated pulse');
     }, {
         offset: '50%'
     });
-    
-    
+
+
     /* mobile nav */
     $('.js--nav-icon').click(function () {
         var nav = $('.js--main-nav');
         var icon = $('.js--nav-icon i');
-        
+
         nav.slideToggle(200);
         if (icon.hasClass('ion-navicon-round')) {
             icon.addClass('ion-close-round');
@@ -114,26 +114,26 @@ $(document).ready(function () {
             icon.addClass('ion-navicon-round');
             icon.removeClass('ion-close-round');
         }
-        
-        
+
+
     });
-    
-    /* Maps */
-   
-    var map = new GMaps({
-      div: '.map',
-      lat: 51.500342,
-      lng: 0,
-      zoom: 12
-    });
-    
-    map.addMarker({
-      lat: 51.500342,
-      lng: -0.111342,
-      title: 'London',
-      infoWindow: {
-        content: '<p>our London HQ</p>'
-      }
-    });
-    
+
+    // /* Maps */
+    //
+    // var map = new GMaps({
+    //   div: '.map',
+    //   lat: 51.500342,
+    //   lng: 0,
+    //   zoom: 12
+    // });
+    //
+    // map.addMarker({
+    //   lat: 51.500342,
+    //   lng: -0.111342,
+    //   title: 'London',
+    //   infoWindow: {
+    //     content: '<p>our London HQ</p>'
+    //   }
+    // });
+
 });
